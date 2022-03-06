b31d53000f70ab8e478ea4e4079c111ec0cd490e
jliveusa <42428798+jliveusa@users.noreply.github.com>
2022-03-06 16:32:34 +0800

    Update and rename diy1.sh to diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# File name: diy1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# File name: diy.sh
+# Description: OpenWrt DIY script part(Before Update feeds)
 #
 
 # Remove telephony
@@ -14,4 +14,4 @@ git clone https://github.com/QiuSimons/openwrt-mos package/more/mosdns
 git clone https://github.com/fw876/helloworld package/more/helloworld
 git clone https://github.com/kuoruan/luci-app-kcptun package/more/luci-app-kcptun
 # git clone https://github.com/kuoruan/luci-app-frpc package/more/luci-app-frpc
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogi
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
