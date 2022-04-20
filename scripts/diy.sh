d740ef06cb65e4b39acc077de85b2635cf820053
jliveusa <42428798+jliveusa@users.noreply.github.com>
2022-04-20 14:32:57 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -9,9 +9,8 @@ sed -i '/telephony/d' feeds.conf.default
 
 # Add other package
 # git clone https://github.com/destan19/OpenAppFilter package/more/OpenAppFilter
-git clone https://github.com/QiuSimons/openwrt-mos package/more/mosdns
-# git clone https://github.com/xiaorouji/openwrt-passwall package/more/openwrt-passwall
-git clone https://github.com/fw876/helloworld package/more/helloworld
-git clone https://github.com/kuoruan/luci-app-kcptun package/more/luci-app-kcptun
+# git clone https://github.com/QiuSimons/openwrt-mos package/more/mosdns
+# git clone https://github.com/kuoruan/luci-app-kcptun package/more/luci-app-kcptun
 # git clone https://github.com/kuoruan/luci-app-frpc package/more/luci-app-frpc
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
