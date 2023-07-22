3520cbad4568e1f76f4942847703a0942efa34f4
jliveusa <42428798+jliveusa@users.noreply.github.com>
2023-07-22 14:13:56 +0800

    Update op_diy.sh

--- scripts/op_diy.sh
+++ scripts/op_diy.sh
@@ -8,6 +8,7 @@
 sed -i '/telephony/d' feeds.conf.default
 
 # Add other package
+git clone https://github.com/jliveusa/openwrt-mod package/more/openwrt-mod
 # git clone https://github.com/destan19/OpenAppFilter package/more/OpenAppFilter
 # git clone https://github.com/kuoruan/luci-app-kcptun package/more/luci-app-kcptun
 # git clone https://github.com/kuoruan/luci-app-frpc package/more/luci-app-frpc
@@ -21,7 +22,11 @@ git clone https://github.com/sbwml/v2ray-geodata package/more/v2ray-geodata
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/more/luci-app-ssr-plus
 svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/more/lua-neturl
 svn co https://github.com/fw876/helloworld/trunk/redsocks2 package/more/redsocks2
-git clone https://github.com/jliveusa/openwrt-mod package/more/openwrt-mod
+svn co https://github.com/kenzok8/small-package/trunk/luci-app-autotimeset package/more/luci-app-autotimeset
+svn co https://github.com/kenzok8/small-package/trunk/luci-app-ddns-go package/more/luci-app-ddns-go
+#svn co https://github.com/kenzok8/small-package/trunk/luci-app-dockerman package/more/luci-app-dockerman
+svn co https://github.com/kenzok8/small-package/trunk/luci-app-fileassistant package/more/luci-app-fileassistant
+
 
 # Set permission
 chmod -R 755 package/more/
