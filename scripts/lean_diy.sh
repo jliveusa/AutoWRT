fa14ceb0a372106b66e2839e6bcb67c55dec159b
jliveusa <42428798+jliveusa@users.noreply.github.com>
2023-10-09 22:55:10 +0800

    Update lean_diy.sh

--- scripts/lean_diy.sh
+++ scripts/lean_diy.sh
@@ -8,20 +8,22 @@
 sed -i '/telephony/d' feeds.conf.default
 
 # Add other package
-# git clone https://github.com/destan19/OpenAppFilter package/more/OpenAppFilter
-# git clone https://github.com/kuoruan/luci-app-kcptun package/more/luci-app-kcptun
-# git clone https://github.com/kuoruan/luci-app-frpc package/more/luci-app-frpc
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/more/passwall-packages
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/more/passwall-luci
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/more/passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/more/passwall-luci
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package/more/passwall2-luci
-# git clone https://github.com/QiuSimons/openwrt-mos package/more/openwrt-mos
-git clone https://github.com/sbwml/luci-app-mosdns package/more/mosdns
+
+git clone https://github.com/jliveusa/luci-app-mosdns -b v5-119 package/more/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/more/v2ray-geodata
-# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/more/luci-app-amlogic
+
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/more/luci-app-ssr-plus
 svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/more/lua-neturl
 svn co https://github.com/fw876/helloworld/trunk/redsocks2 package/more/redsocks2
-git clone https://github.com/jliveusa/openwrt-mod package/more/openwrt-mod
+
+git clone https://github.com/izilzty/luci-app-chinadns-ng package/more/luci-app-chinadns-ng
+git clone https://github.com/izilzty/openwrt-chinadns-ng.git package/more/chinadns-ng
+
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-ddns-go package/more/luci-app-ddns-go
+svn co https://github.com/kiddin9/openwrt-packages/trunk/ddns-go package/more/ddns-go
 
 # Set permission
 chmod -R 755 package/more/
