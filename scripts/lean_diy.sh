fa07f609f5d4aa8a26898f181505a3db7389a9af
jliveusa <42428798+jliveusa@users.noreply.github.com>
2023-12-17 20:52:22 +0800

    Update lean_diy.sh

--- scripts/lean_diy.sh
+++ scripts/lean_diy.sh
@@ -18,6 +18,8 @@ git clone https://github.com/sbwml/v2ray-geodata package/more/v2ray-geodata
 svn co https://github.com/fw876/helloworld/branches/main/luci-app-ssr-plus package/more/luci-app-ssr-plus
 svn co https://github.com/fw876/helloworld/branches/main/lua-neturl package/more/lua-neturl
 svn co https://github.com/fw876/helloworld/branches/main/redsocks2 package/more/redsocks2
+svn co https://github.com/fw876/helloworld/branches/main/shadow-tls package/more/shadow-tls
+svn co https://github.com/fw876/helloworld/branches/main/gn package/more/gn
 
 git clone https://github.com/izilzty/luci-app-chinadns-ng package/more/luci-app-chinadns-ng
 git clone https://github.com/jliveusa/openwrt-chinadns-ng.git package/more/chinadns-ng
