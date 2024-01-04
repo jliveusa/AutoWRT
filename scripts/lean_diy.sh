5dface815e4578039397c73bf95b316f0375ba96
jliveusa <42428798+jliveusa@users.noreply.github.com>
2024-01-04 19:18:08 +0800

    Update lean_diy.sh

--- scripts/lean_diy.sh
+++ scripts/lean_diy.sh
@@ -15,10 +15,10 @@ git clone https://github.com/xiaorouji/openwrt-passwall2.git package/more/passwa
 git clone https://github.com/jliveusa/luci-app-mosdns -b v5-119 package/more/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/more/v2ray-geodata
 
-svn co https://github.com/fw876/helloworld/branches/main/luci-app-ssr-plus package/more/luci-app-ssr-plus
-svn co https://github.com/fw876/helloworld/branches/main/lua-neturl package/more/lua-neturl
-svn co https://github.com/fw876/helloworld/branches/main/redsocks2 package/more/redsocks2
-svn co https://github.com/fw876/helloworld/branches/main/shadow-tls package/more/shadow-tls
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/more/luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/more/lua-neturl
+svn co https://github.com/fw876/helloworld/trunk/redsocks2 package/more/redsocks2
+svn co https://github.com/fw876/helloworld/trunk/shadow-tls package/more/shadow-tls
 
 git clone https://github.com/izilzty/luci-app-chinadns-ng package/more/luci-app-chinadns-ng
 git clone https://github.com/jliveusa/openwrt-chinadns-ng.git package/more/chinadns-ng
