60a69818e1a7b857b0c64435c296b7c0578a873c
jliveusa <42428798+jliveusa@users.noreply.github.com>
2024-01-06 17:39:34 +0800

    Update lean_diy.sh

--- scripts/lean_diy.sh
+++ scripts/lean_diy.sh
@@ -15,6 +15,10 @@ git clone https://github.com/xiaorouji/openwrt-passwall2.git package/more/passwa
 git clone https://github.com/jliveusa/luci-app-mosdns -b v5-119 package/more/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/more/v2ray-geodata
 
+svn co https://github.com/v2rayA/v2raya-openwrt/trunk/luci-app-v2raya package/more/luci-app-v2raya
+svn co https://github.com/v2rayA/v2raya-openwrt/trunk/v2raya package/more/v2raya
+svn co https://github.com/v2rayA/v2raya-openwrt/trunk/v2fly-geodata package/more/v2fly-geodata
+
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/more/luci-app-ssr-plus
 svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/more/lua-neturl
 svn co https://github.com/fw876/helloworld/trunk/redsocks2 package/more/redsocks2
