d3ddc8f4e3cf0fb4e1de72c89808b72ec07c7365
jliveusa <42428798+jliveusa@users.noreply.github.com>
2024-02-16 14:02:23 +0800

    Update op_diy.sh

--- scripts/op_diy.sh
+++ scripts/op_diy.sh
@@ -8,6 +8,41 @@
 sed -i '/telephony/d' feeds.conf.default
 
 # Add other package
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/more/passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall package/more/passwall-luci
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/more/passwall2-luci
+git clone https://github.com/sbwml/luci-app-mosdns package/more/luci-app-mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/more/v2ray-geodata
+
+# Merge_package
+function merge_package() {
+    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    if [[ $# -lt 3 ]]; then
+        echo "Syntax error: [$#] [$*]" >&2
+        return 1
+    fi
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    # 使用循环逐个移动文件夹
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+
+merge_package master https://github.com/kiddin9/openwrt-packages package/more ddns-go luci-app-ddns-go
+merge_package master https://github.com/fw876/helloworld package/more redsocks2 shadow-tls lua-neturl luci-app-ssr-plus
+
+# Add other package
 
 # git clone https://github.com/destan19/OpenAppFilter package/more/OpenAppFilter
 # git clone https://github.com/kuoruan/luci-app-kcptun package/more/luci-app-kcptun
