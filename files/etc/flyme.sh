62572a0457090a77815814c15e81523445d45615
James <42428798+jliveusa@users.noreply.github.com>
2024-04-20 19:42:44 +0800

    Update flyme.sh

--- files/etc/flyme.sh
+++ files/etc/flyme.sh
@@ -3,15 +3,16 @@ PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
 # update frp
 # mkdir -p /tmp/frp && cd /tmp/frp
-# wget -c -T 0 --no-check-certificate https://github.com/jliveusa/AutoWRT/releases/download/frp/frpc_amd64
-# chmod 755 frpc_amd64
+# wget -c -T 0 --no-check-certificate https://github.com/jliveusa/AutoWRT/releases/download/frp/frpc_amd64 -O frpc
+# chmod 755 frpc
 # rm -f /usr/bin/frpc
-# mv -f frpc_amd64 /usr/bin/frpc
+# mv -f frpc /usr/bin/frpc
 # cd ~ && rm -rf /tmp/frp
 # /etc/init.d/frp restart
  
 # update dnsmasq
-# wget -T 0 --no-check-certificate https://github.com/felixonmars/dnsmasq-china-list/blob/master/accelerated-domains.china.conf -O /tmp/accelerated-domains.china.conf.tmp
+# cd /tmp
+# wget -T 0 --no-check-certificate https://github.com/felixonmars/dnsmasq-china-list/blob/master/accelerated-domains.china.conf -O accelerated-domains.china.conf.tmp
 # mv -f /tmp/accelerated-domains.china.conf.tmp /etc/dnsmasq.d/accelerated-domains.china.conf
 # /etc/init.d/dnsmasq restart
 
@@ -27,18 +28,18 @@ PATH="/usr/sbin:/usr/bin:/sbin:/bin"
  
 # update xray
  mkdir -p /tmp/xray && cd /tmp/xray
- wget -c -T 0 --no-check-certificate https://github.com/jliveusa/AutoWRT/releases/download/xray/xray_amd64
- chmod 755 xray_amd64
+ wget -c -T 0 --no-check-certificate https://github.com/jliveusa/AutoWRT/releases/download/xray/xray_amd64 -O xray
+ chmod 755 xray
  rm -f /usr/bin/xray
- mv -f xray_amd64 /usr/bin/xray
+ mv -f xray /usr/bin/xray
  cd ~ && rm -rf /tmp/xray
 
 # update v2ray
 # mkdir -p /tmp/v2ray && cd /tmp/v2ray
-# wget -c -T 0 --no-check-certificate https://github.com/jliveusa/AutoWRT/releases/download/v2ray/v2ray_amd64
-# chmod 755 v2ray_amd64
+# wget -c -T 0 --no-check-certificate https://github.com/jliveusa/AutoWRT/releases/download/v2ray/v2ray_amd64 -O v2ray
+# chmod 755 v2ray
 # rm -f /usr/bin/v2ray
-# mv -f v2ray_amd64 /usr/bin/v2ray
+# mv -f v2ray /usr/bin/v2ray
 # cd ~ && rm -rf /tmp/v2ray
  
 # /etc/init.d/passwall restart
