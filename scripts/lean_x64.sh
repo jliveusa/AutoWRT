42a68bc5e5e62a45c2698eefb022d9ab6f770317
James <42428798+jliveusa@users.noreply.github.com>
2024-11-24 20:12:38 +0800

    Update lean_x64.sh

--- scripts/lean_x64.sh
+++ scripts/lean_x64.sh
@@ -10,6 +10,8 @@ sed -i 's/192.168.1.1/192.168.50.57/g' package/base-files/files/bin/config_gener
 # Remove unused ipk
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-ddns-go
+rm -rf feeds/packages/net/ddns-go
 rm -rf feeds/packages/lang/golang
 
 # Modify ipk
