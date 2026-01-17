9aa3c5f9afffffecd553876655015bb4155fbeec
James <42428798+jliveusa@users.noreply.github.com>
2026-01-17 17:37:45 +0800

    Update flyme.sh

--- files/etc/flyme.sh
+++ files/etc/flyme.sh
@@ -3,7 +3,7 @@ PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 
 # update xray
  mkdir -p /tmp/xray && cd /tmp/xray
- wget -c -T 0 --no-check-certificate https://github.com/jliveusa/AutoWRT/releases/download/xray/xray_amd64
+ wget -c -T 0 --no-check-certificate https://github.com/jliveusa/AutoWRT/releases/download/xray/xray_amd64 -O xray_amd64
   if [ -f "/tmp/xray/xray_amd64" ]; then
      chmod 755 xray_amd64 && rm -f /usr/bin/xray && mv -f xray_amd64 /usr/bin/xray
      echo "succeed" 
